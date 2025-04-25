e29c0599045321f00a6f4faa409d735a4bfbac59
Sounak Saha <sounaksaha@Mac.lan>
2025-04-25 22:03:32 +0530

    get district

--- src/controllers/districtController.js
+++ src/controllers/districtController.js
@@ -31,11 +31,12 @@ export const createDistrict = async (req, res) => {
 
 export const getDistricts = async (req, res) => {
   try {
-    const { searchQuery, skip, limit, page } = getQueryOptions(req, 'districtName');
+    const { searchQuery, skip, limit, page } = getQueryOptions(
+      req,
+      "districtName"
+    );
 
-    const districts = await District.find(searchQuery)
-      .skip(skip)
-      .limit(limit);
+    const districts = await District.find(searchQuery).skip(skip).limit(limit);
 
     const total = await District.countDocuments(searchQuery);
 
@@ -46,12 +47,107 @@ export const getDistricts = async (req, res) => {
         perPage: limit,
         currentCount: districts.length, // 👈 shows how many items were returned on this page
         totalPages: Math.ceil(total / limit),
-        totalItems: total
+        totalItems: total,
       })
     );
   } catch (err) {
-    res.status(500).json(
-      new ApiResponse(false, 500, "server error", { message: err.message })
-    );
+    res
+      .status(500)
+      .json(
+        new ApiResponse(false, 500, "server error", { message: err.message })
+      );
+  }
+};
+
+export const getDistrictById = async (req, res) => {
+  try {
+    const { id } = req.query;
+    const existing = await District.findById(id);
+    if (!existing) {
+      return res
+        .status(404)
+        .json(new ApiResponse(false, 404, "No District Found"));
+    }
+    return res
+      .status(200)
+      .json(new ApiResponse(true, 200, "District Details", existing));
+  } catch (error) {
+    return res
+      .status(500)
+      .json(
+        new ApiResponse(false, 500, "server error", { message: err.message })
+      );
+  }
+};
+
+export const updateDistrictById = async (req, res) => {
+  const { id } = req.query;
+  const { code, name } = req.body;
+
+  try {
+    const district = await District.findById(id);
+    if (!district) {
+      return res
+        .status(404)
+        .json(new ApiResponse(false, 404, "District not found"));
+    }
+    if (code) {
+      const existing = await District.findOne({
+        districtCode: code,
+        _id: { $ne: id },
+      });
+      if (existing) {
+        return res
+          .status(400)
+          .json(
+            new ApiResponse(
+              false,
+              400,
+              "Another district with the same code already exists"
+            )
+          );
+      }
+    }
+    if (code) district.districtCode = code;
+    if (name) district.districtName = name;
+
+    await district.save();
+
+    res
+      .status(200)
+      .json(
+        new ApiResponse(true, 200, "District updated successfully", district)
+      );
+  } catch (error) {
+    res
+      .status(500)
+      .json(
+        new ApiResponse(false, 500, "Server Error", { error: error.message })
+      );
+  }
+};
+
+export const deleteDistrictById = async (req, res) => {
+  try {
+    const { id } = req.query;
+    const existing = await District.findById(id);
+    if (!existing) {
+      return res
+        .status(404)
+        .json(new ApiResponse(false, 404, "No District Found"));
+    }
+    const deleteData = await District.findByIdAndDelete(id);
+
+    if (deleteData) {
+      return res
+        .status(200)
+        .json(new ApiResponse(true, 200, "Delete Successfull", deleteData));
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json(
+        new ApiResponse(false, 500, "server error", { message: err.message })
+      );
   }
 };
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { registerUser } from '../controllers/authController.js';
-import { createDistrict, getDistricts  } from '../controllers/districtController.js';
+import { createDistrict, deleteDistrictById, getDistrictById, getDistricts, updateDistrictById  } from '../controllers/districtController.js';
 import { verifyToken, authorizeRoles } from '../middleware/auth.js';
 import validate from '../middleware/validate.js';
 import { validateDistrict, validateRegistration } from '../utils/validator.js';
@@ -12,7 +12,9 @@ router.post('/register-user', verifyToken, authorizeRoles('admin'), validateRegi
 router.post('/create-district', verifyToken, authorizeRoles('admin'),validateDistrict,validate, createDistrict);
 router.get('/districts', verifyToken, authorizeRoles('admin'), getDistricts);
 router.post('/create-subdistrict', verifyToken, authorizeRoles('admin'),validateDistrict,validate, createSubDistrict);
-router.get('/subdistrict', verifyToken, authorizeRoles('admin'),validateDistrict,validate, getSubDistricts);
-
+router.get('/subdistricts', verifyToken, authorizeRoles('admin'), getSubDistricts);
+router.get('/district', verifyToken, authorizeRoles('admin'), getDistrictById );
+router.put('/district', verifyToken, authorizeRoles('admin'),validateDistrict,validate, updateDistrictById);
+router.delete('/district', verifyToken, authorizeRoles('admin'), deleteDistrictById);
 
 export default router;
