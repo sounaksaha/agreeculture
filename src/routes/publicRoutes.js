4821aa5887e1a2f74849015ebe14033a6d3e6fb0
yashrajput0508 <yasharajput0508@gmail.com>
2025-05-03 00:25:31 +0530

    dashboard Controller

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -1,13 +1,31 @@
 import express from "express";
 import { authorizeRoles, verifyToken } from "../middleware/auth.js";
-import {  getAllFarmers, getFarmerById } from "../controllers/farmerController.js";
+import {
+  getAllFarmers,
+  getFarmerById,
+} from "../controllers/farmerController.js";
+import { getDashboard } from "../controllers/dashboardController.js";
 
 const router = express.Router();
 
-router.get("/get-farmer",verifyToken,authorizeRoles('user','admin'),getAllFarmers);
-router.get("/farmer-detail",verifyToken,authorizeRoles('user','admin'),getFarmerById);
+router.get(
+  "/get-farmer",
+  verifyToken,
+  authorizeRoles("user", "admin"),
+  getAllFarmers
+);
+router.get(
+  "/farmer-detail",
+  verifyToken,
+  authorizeRoles("user", "admin"),
+  getFarmerById
+);
 
-
-
+router.get(
+  "/dashboard",
+  verifyToken,
+  authorizeRoles("user", "admin"),
+  getDashboard
+);
 
 export default router;
